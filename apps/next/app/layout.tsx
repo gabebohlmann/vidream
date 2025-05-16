43d7d325debc7ce71d58f50029851233a3daf94c
Gabe Bohlmann <gabebohlmann@gmail.com>
2025-05-16 18:15:41 -0500

    Add Convex and Clerk providers for Next

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -1,6 +1,15 @@
 // apps/next/app/layout.tsx
 import type { Metadata } from 'next'
 import { NextTamaguiProvider } from 'app/provider/NextTamaguiProvider'
+import { ConvexReactClient } from 'convex/react'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
+import { ClerkProvider, useAuth } from '@clerk/nextjs'
+
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+  throw new Error('Missing NEXT_PUBLIC_CONVEX_URL in your .env file')
+}
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL)
 
 export const metadata: Metadata = {
   title: 'Tamagui • App Router',
@@ -13,7 +22,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     // You can use `suppressHydrationWarning` to avoid the warning about mismatched content during hydration in dev mode
     <html lang="en" suppressHydrationWarning>
       <body>
-        <NextTamaguiProvider>{children}</NextTamaguiProvider>
+        <ClerkProvider>
+          <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+            <NextTamaguiProvider>{children}</NextTamaguiProvider>
+          </ConvexProviderWithClerk>
+        </ClerkProvider>
       </body>
     </html>
   )
