ebe4d129f04ca617f12b0c951727d52677d56dc1
Gabe Bohlmann <gabebohlmann@gmail.com>
2025-05-16 21:23:34 -0500

    Add ConvexProviderWithClerk

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -2,14 +2,12 @@
 import type { Metadata } from 'next'
 import { NextTamaguiProvider } from 'app/provider/NextTamaguiProvider'
 import { ConvexReactClient } from 'convex/react'
-import { ConvexProviderWithClerk } from 'convex/react-clerk'
-import { ClerkProvider, useAuth } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs'
+import ConvexClientProviderWithClerk from '../components/ConvexProviderWithClerk'
 
 if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
   throw new Error('Missing NEXT_PUBLIC_CONVEX_URL in your .env file')
 }
-
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL)
 
 export const metadata: Metadata = {
   title: 'Tamagui • App Router',
@@ -20,12 +18,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     // You can use `suppressHydrationWarning` to avoid the warning about mismatched content during hydration in dev mode
-    <html lang="en" suppressHydrationWarning>
+    // <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body>
         <ClerkProvider>
-          <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+          <ConvexClientProviderWithClerk>
             <NextTamaguiProvider>{children}</NextTamaguiProvider>
-          </ConvexProviderWithClerk>
+          </ConvexClientProviderWithClerk>
         </ClerkProvider>
       </body>
     </html>
