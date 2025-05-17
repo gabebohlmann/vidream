00bef727341557c2d4749d1369976a88c565677a
Gabe Bohlmann <gabebohlmann@gmail.com>
2025-05-17 10:57:22 -0500

    Add Next home page

--- apps/next/app/page.tsx
+++ apps/next/app/page.tsx
@@ -1,6 +1,14 @@
 // apps/next/app/page.tsx
 'use client'
 
-import { HomeScreen } from 'app/features/home/screen'
+// import { HomeScreen } from 'app/features/home/screen'
+import { Link } from 'solito/link'
 
-export default HomeScreen
+// export default HomeScreen
+export default function HomePage() {
+  return (
+    <>
+      <Link href="/sign-in">Sign In</Link>
+    </>
+  )
+}
