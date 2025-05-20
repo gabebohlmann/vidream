5abd285a385e663e52a3689c15c3de066fa2f9b0
Gabe Bohlmann <gabebohlmann@gmail.com>
2025-05-20 15:40:15 -0500

    Remove old templating

--- apps/next/app/(auth)/sign-up/page.tsx
+++ apps/next/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,5 @@
 // apps/next/app/(auth)/sign-up/page.tsx
-// import { AuthLayout } from 'app/features/auth/layout.web'
 import SignUpScreen from 'app/features/auth/sign-up-screen'
-// import Head from 'next/head'
 
 export const metadata = {
   title: 'Sign Up',
