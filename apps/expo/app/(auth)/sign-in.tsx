39c970ff2f4555f56870a81c4610a085e4d7010c
Gabe Bohlmann <gabebohlmann@gmail.com>
2025-05-20 16:11:13 -0500

    Remove old templating

--- apps/expo/app/(auth)/sign-in.tsx
+++ apps/expo/app/(auth)/sign-in.tsx
@@ -1,7 +1,5 @@
 // apps/expo/app/(auth)/sign-in.tsx
 import SignInScreen from 'app/features/auth/sign-in/screen'
-import { Stack } from 'expo-router'
-import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Screen() {
   return <SignInScreen />
