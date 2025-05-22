c9d3a2240bbcb18b3216fd131bfcc4342e6c859b
Gabe Bohlmann <gabebohlmann@gmail.com>
2025-05-22 05:52:06 -0500

    Update import paths

--- apps/expo/app/(auth)/sign-in.tsx
+++ apps/expo/app/(auth)/sign-in.tsx
@@ -1,5 +1,5 @@
 // apps/expo/app/(auth)/sign-in.tsx
-import SignInScreen from 'app/features/auth/sign-in/screen'
+import SignInScreen from 'app/features/auth/sign-in/screen.native'
 
 export default function SignInPage() {
   return <SignInScreen />
