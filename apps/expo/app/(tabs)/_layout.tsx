1e8d033c3bc87341ddcf23488c8f86077b5c9265
Gabe Bohlmann <gabebohlmann@gmail.com>
2025-05-22 05:29:47 -0500

    Remove ripple animation

--- apps/expo/app/(tabs)/_layout.tsx
+++ apps/expo/app/(tabs)/_layout.tsx
@@ -2,17 +2,37 @@
 import { Tabs } from 'expo-router'
 import { Home, TvMinimalPlay, Search, Film, CircleUserRound } from '@tamagui/lucide-icons'
 import { TopNavBar } from 'app/features/navigation/TopNavBar.native'
+import { Pressable } from 'react-native'
+
+const TabBarButton = (props: PressableProps) => {
+  return (
+    <Pressable
+      {...props} // Pass down all original props (onPress, accessibilityState, children, style, etc.)
+      android_ripple={{ color: 'transparent' }} // Disables the ripple effect on Android
+      // For iOS, providing a style prop (even if it's just the original style)
+      // overrides the default behavior of dimming/lightening the view on press.
+      style={props.style}
+    />
+  )
+}
 
 export default function TabLayout() {
   return (
     <>
-      <TopNavBar/>
-      <Tabs screenOptions={{ tabBarActiveTintColor: '#138404', tabBarInactiveTintColor: '#FFFFFF', headerShown: false }}>
+      <TopNavBar />
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: '#138404',
+          tabBarInactiveTintColor: '#FFFFFF',
+          headerShown: false,
+          tabBarButton: (props) => <TabBarButton {...props} />,
+        }}
+      >
         <Tabs.Screen
           name="index"
           options={{
             title: 'Home',
-            tabBarIcon: ({ color }) => <Home size={28} color={color} />,
+            tabBarIcon: ({ color }) => <Home size={28} color={color} strokeWidth={1.5} />,
             tabBarLabelStyle: {
               fontSize: 9,
               fontWeight: 'bold',
@@ -23,7 +43,7 @@ export default function TabLayout() {
           name="flashes"
           options={{
             title: 'Flashes',
-            tabBarIcon: ({ color }) => <Film size={28} color={color} />,
+            tabBarIcon: ({ color }) => <Film size={28} color={color} strokeWidth={1.5} />,
             tabBarLabelStyle: {
               fontSize: 9,
               fontWeight: 'bold',
@@ -34,7 +54,7 @@ export default function TabLayout() {
           name="search"
           options={{
             title: 'Search',
-            tabBarIcon: ({ color }) => <Search size={28} color={color} />,
+            tabBarIcon: ({ color }) => <Search size={28} color={color} strokeWidth={1.5} />,
             tabBarLabelStyle: {
               fontSize: 9,
               fontWeight: 'bold',
@@ -45,7 +65,7 @@ export default function TabLayout() {
           name="subscriptions"
           options={{
             title: 'Subscriptions',
-            tabBarIcon: ({ color }) => <TvMinimalPlay size={28} color={color} />,
+            tabBarIcon: ({ color }) => <TvMinimalPlay size={28} color={color} strokeWidth={1.5} />,
             tabBarLabelStyle: {
               fontSize: 9,
               fontWeight: 'bold',
@@ -56,7 +76,9 @@ export default function TabLayout() {
           name="account"
           options={{
             title: 'Account',
-            tabBarIcon: ({ color }) => <CircleUserRound size={28} color={color} />,
+            tabBarIcon: ({ color }) => (
+              <CircleUserRound size={28} color={color} strokeWidth={1.5} />
+            ),
             tabBarLabelStyle: {
               fontSize: 9,
               fontWeight: 'bold',
